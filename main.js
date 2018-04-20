441d0b5a69427f838855596351eb5f56abcc6fd8
Christian Aracena <CDAracena@gmail.com>
2018-04-19 23:54:17 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,10 @@
+/*let allBoxes = document.querySelectorAll('.boxes1');
+let butTon = document.querySelector('#theButton');
+
+butTon.addEventListener('mouseover', function() {
+  for (let i=0; i < allBoxes.length; i++) {
+  allBoxes[i].style.backgroundColor='green';
+  allBoxes[i].innerText = "I changed this text!";
+ }
+});
+*/
